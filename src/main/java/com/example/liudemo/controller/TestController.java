fb81c04c5d8762c689508624b7863b58efa32593
liuzhuopeng <lzhuopeng@163.com>
2019-12-07 11:25:42 +0800

    测试提交

--- src/main/java/com/example/liudemo/controller/TestController.java
+++ src/main/java/com/example/liudemo/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.example.liudemo.controller;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * @author lzp
@@ -16,6 +22,33 @@ public class TestController {
     @RequestMapping("/hello")
     public String helloWorld(){
         return "liu,hello";
+    }
+
+    @ResponseBody
+    @RequestMapping("/git")
+    public String queryGitHub() {
+        try {
+            URL url = new URL("https://api.github.com/users/Vitaminliu/repos");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setDoOutput(true); // 设置该连接是可以输出的
+            connection.setRequestMethod("GET"); // 设置请求方式
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
+            String line = null;
+            StringBuilder result = new StringBuilder();
+            while ((line = br.readLine()) != null) { // 读取数据
+                result.append(line + "\n");
+            }
+            connection.disconnect();
+
+            System.out.println(result.toString());
+            return result.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
